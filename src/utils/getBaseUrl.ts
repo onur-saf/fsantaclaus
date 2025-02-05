e06c501035a91502b012e60e5f94f74a311933d3
Onur Saf <onur.saf@ready2.gg>
2025-02-05 17:24:52 +0300

    add env vars

--- src/utils/getBaseUrl.ts
+++
@@ -1,5 +0,0 @@
-export function getBaseUrl() {
-  return process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : process.env.NEXT_PUBLIC_BASE_URL || 'http://fsantaclaus.us/';
-}
